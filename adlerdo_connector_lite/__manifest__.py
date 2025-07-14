eec728238a8c5dd573bd203a161fd96bb8de7e1c
Abdulrahman-mohamed-Alfa <abdelrahman@alfa-software.com>
2025-07-14 14:32:05 +0300

    Update __manifest__.py

--- adlerdo_connector_lite/__manifest__.py
+++ adlerdo_connector_lite/__manifest__.py
@@ -4,23 +4,7 @@
 
     'summary': "marketplace reporting & lead generation tool for Odoo",
 
-    'description': """
-
-A Central Solution for Amazon, eBay, Kaufland & More.
-
-With the **AdlaDoo Middleware**, you manage your product, order, and inventory data across all marketplaces – reliably, automatically, and fully under your control.
-
-Installed on your own Windows system, AdlaDoo offers **maximum data security**, **full transparency**, and **significant time savings** in your daily operations.
-
-No cloud dependency, no data leaks, no compromises.
-Just one powerful tool that truly simplifies your multichannel sales – whether you're selling on Amazon, eBay, Kaufland, Otto, or other platforms.
-
-**AdlaDoo is more than just an interface** – it's your **central control unit** for all marketplace processes:
-from automated order processing to price and stock synchronization with your ERP system.
-
-Ideal for online retailers, wholesalers, and ERP-based businesses that want to grow – without giving up control of their operations.
-
-    """,
+    'description': "marketplace reporting & lead generation tool for Odoo",
 
     'author': "TecSee GmbH",
     'website': "https://www.tecsee.de",
@@ -31,7 +15,7 @@ Ideal for online retailers, wholesalers, and ERP-based businesses that want to g
     'depends': ['base'],
     'images' : ['static/description/banner.png'],
 
-    'license': 'Other proprietary',
+    'license': '	LGPL-3',
     'application': True,
     'installable': True,
 }
